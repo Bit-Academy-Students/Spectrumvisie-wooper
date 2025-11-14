c73e923ebfb7cc8e4b5221948e101998b3f674b3
Siilvan <silvan.groenveld@gmail.com>
2025-11-14 12:47:53 +0100

    start migration

--- SpectrumVisie/database/migrations/2025_11_14_113708_create_materiaal_table.php
+++ SpectrumVisie/database/migrations/2025_11_14_113708_create_materiaal_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('materiaal', function (Blueprint $table) {
             $table->id();
+<<<<<<< HEAD
             $table->string('title')->nullable();
             $table->string('description');
             $table->foreignId('material_type_id')->constrained(table: 'material_type', column: 'id')->onDelete('cascade');
@@ -20,6 +21,8 @@ return new class extends Migration
             $table->string('file_path')->nullable();
             $table->dateTime('uploaded_at')->useCurrent();
 
+=======
+>>>>>>> ae545a6 (start migration)
             $table->timestamps();
         });
     }
