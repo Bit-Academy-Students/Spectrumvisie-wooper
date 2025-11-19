7bbea4cef1dda3416028d15940a58ce98a848c5e
JayvenAltenberg <jayvenaltenberg@gmail.com>
2025-11-19 14:59:53 +0100

    login working and added the PendingController

--- SpectrumVisie/app/Http/Controllers/PendingController.php
+++ SpectrumVisie/app/Http/Controllers/PendingController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+<<<<<<< HEAD
 use App\Models\PendingUser;
+=======
+>>>>>>> ad6f24c (login working and added the PendingController)
 use Illuminate\Http\Request;
 
 class PendingController extends Controller
 {
+<<<<<<< HEAD
     public function ShowAllUsers()
     {
         return PendingUser::all();
     }
+=======
+    //
+>>>>>>> ad6f24c (login working and added the PendingController)
 }
