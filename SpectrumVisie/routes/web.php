9790a626c0eeb04ab9f35c9f5f7d0d4bbc79cc3b
Siilvan <silvan.groenveld@gmail.com>
2025-11-19 10:59:53 +0100

    start uploading material

--- SpectrumVisie/routes/web.php
+++ SpectrumVisie/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MateriaalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/upload', [MateriaalController::class, 'showUploadForm'])->name('upload.form');
+Route::post('/upload', [MateriaalController::class, 'upload'])->name('upload.post');
