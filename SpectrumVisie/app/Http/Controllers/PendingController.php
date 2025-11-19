8d6d5738f6f67b08ed3690efdd7a20bf9ae671ac
JayvenAltenberg <jayvenaltenberg@gmail.com>
2025-11-19 15:54:25 +0100

    shows the people pending to have a account

--- SpectrumVisie/app/Http/Controllers/PendingController.php
+++ SpectrumVisie/app/Http/Controllers/PendingController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\PendingUser;
-=======
->>>>>>> ad6f24c (login working and added the PendingController)
 use Illuminate\Http\Request;
 
 class PendingController extends Controller
 {
-<<<<<<< HEAD
     public function ShowAllUsers()
     {
         return PendingUser::all();
     }
-=======
-    //
->>>>>>> ad6f24c (login working and added the PendingController)
 }
