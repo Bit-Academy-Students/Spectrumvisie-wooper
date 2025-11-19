b1f816b62aad11fcf7cef97ed29b193999db9a35
Siilvan <silvan.groenveld@gmail.com>
2025-11-19 15:20:57 +0100

    uploading works with errors

--- SpectrumVisie/app/Http/Controllers/MateriaalController.php
+++ SpectrumVisie/app/Http/Controllers/MateriaalController.php
@@ -14,6 +14,21 @@ class MateriaalController extends Controller
         return view('upload', compact('types'));
     }
 
+    protected function youtubeEmbedUrl($url)
+    {
+        if (preg_match('/youtu\.be\/([^\?]+)/', $url, $matches)) {
+            $id = $matches[1];
+        }
+        elseif (preg_match('/v=([^\&]+)/', $url, $matches)) {
+            $id = $matches[1];
+        } else {
+            return null;
+        }
+
+        return "https://www.youtube.com/embed/$id";
+    }
+
+
     public function upload(Request $request)
     {
         $validated = $request->validate([
@@ -26,18 +41,63 @@ class MateriaalController extends Controller
 
         $filePath = null;
 
+        $materialType = MaterialType::find($validated['material_type_id']);
+        $type = $materialType->type;
+
+        $allowedExtensions = [
+            'pdf' => ['pdf'],
+            'word' => ['doc', 'docx'],
+            'video' => ['mp4', 'mov', 'avi', 'webm'],
+            'youtube-link' => [],
+        ];
+
         if ($request->hasFile('file')) {
-            $filePath = $request->file('file')->store('materials', 'public');
+            $file = $request->file('file');
+            $ext = strtolower($file->getClientOriginalExtension());
+
+            if (!in_array($ext, $allowedExtensions[$type])) {
+                return redirect()->back()
+                    ->withErrors(['file' => 'Ongeldig bestandstype voor geselecteerd materiaal type.'])
+                    ->withInput();
+            }
+
+            if (filled($validated['youtube_url'])) {
+                return redirect()->back()
+                    ->withErrors(['file' => 'Een link is niet toegestaan voor dit materiaal type.'])
+                    ->withInput();
+            }
+            $filePath = $file->store('materials', 'public');
+        } else {
+            $filePath = null;
+
+            if ($type !== 'youtube-link') {
+                return redirect()->back()
+                    ->withErrors(['file' => 'Bestand is verplicht voor dit materiaal type.'])
+                    ->withInput();
+            }
         }
 
-        Materiaal::create([
+        $youtubeEmbed = null;
+        if ($type === 'youtube-link' && !empty($validated['youtube_url'])) {
+            $youtubeEmbed = $this->youtubeEmbedUrl($validated['youtube_url']);
+            if (!$youtubeEmbed) {
+                throw \Illuminate\Validation\ValidationException::withMessages([
+                    'youtube_url' => 'Ongeldige YouTube URL.',
+                ]);
+            }
+        }
+
+
+        $materiaal = Materiaal::create([
             'title' => $validated['title'] ?? null,
             'description' => $validated['description'],
             'material_type_id' => $validated['material_type_id'],
-            'youtube_url' => $validated['youtube_url'] ?? null,
+            'youtube_url' => $youtubeEmbed ?? null,
             'file_path' => $filePath,
         ]);
 
-        return redirect()->back()->with('success', 'Upload succesvol!');
+        return redirect()->back()
+            ->with('success', 'Upload succesvol!')
+            ->with('uploaded', $materiaal);
     }
 }
