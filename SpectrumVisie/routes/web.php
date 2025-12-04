092a51dde5d4e319bb3c9191f028b73fb114e151
JayvenAltenberg <jayvenaltenberg@gmail.com>
2025-12-04 11:31:17 +0100

    added middleware and logout

--- SpectrumVisie/routes/web.php
+++ SpectrumVisie/routes/web.php
@@ -7,15 +7,16 @@ use App\Http\Controllers\PendingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MateriaalController;
 use App\Http\Controllers\OverzichtController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/register', function (RegisterController $controller) {
     $roles = $controller->ShowRole();
     return view('register', compact('roles'));
-});
+})->middleware("guest");
 
 Route::get('/login', function () {
     return view('login');
-});
+})->middleware("guest");
 
 
 Route::get('/upload', function (MateriaalController $controller) {
@@ -49,6 +50,7 @@ Route::get('/', function () {
 
 Route::post('/register', [RegisterController::class, 'Register']);
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::post('/upload', [MateriaalController::class, 'upload'])->name('upload.post');
 Route::post('/pending/accept/{id}', [PendingController::class, 'AcceptUser'])->name('pending.accept');
 Route::post('/pending/reject/{id}', [PendingController::class, 'RejectUser'])->name('pending.reject');
@@ -60,4 +62,4 @@ Route::get('/admin/dashboard', function (MateriaalController $controller, Pendin
     $UserData = $UserController->ShowAllPendingUsers();
 
     return view('Admin_dashboard', compact('data', 'UserData'));
-})->name('admin.dashboard');
+})->name('admin.dashboard')->middleware(AdminMiddleware::class);
